1cc6f9a257328935c9b76e7eea7edd85d4efbaec
testethalysson1-lgtm <testethalysson1@gmail.com>
2026-04-04 11:57:35 -0300

    atualizaBVBVBVção

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
         'https://i.imgur.com/ChkFVNG.jpeg',
         'https://i.imgur.com/ALZNfiQ.jpeg'
       ],
-      checkout: 'https://blacknosepay.com/pay/1ICXS7CJ',
+      checkout: 'https://blacknosepay.com/pay/PFHHC9CQ',
     },
     {
       id: 4,
@@ -80,7 +80,7 @@ export default function Page() {
         'https://i.imgur.com/UZLuCvy.png',
         'https://i.imgur.com/XZVNLax.png'
       ],
-      checkout: 'https://blacknosepay.com/pay/D141UDE3',
+      checkout: 'https://blacknosepay.com/pay/E4DW9P4Q',
     },
     {
       id: 5,
@@ -97,7 +97,7 @@ export default function Page() {
         'https://i.imgur.com/CHZ80bI.jpeg',
         'https://i.imgur.com/QD6KbRZ.jpeg'
       ],
-      checkout: 'https://blacknosepay.com/pay/EW89UCXP',
+      checkout: 'https://blacknosepay.com/pay/S2QC8O3C',
     },
     {
       id: 6,
@@ -111,7 +111,7 @@ export default function Page() {
         'https://i.imgur.com/HV7a8gj.png',
         'https://i.imgur.com/BThTm0i.png'
       ],
-      checkout: 'https://blacknosepay.com/pay/IQNCRTRP',
+      checkout: 'https://blacknosepay.com/pay/31TH0GE7',
     },
     {
       id: 7,
@@ -126,7 +126,7 @@ export default function Page() {
         'https://i.imgur.com/6rnHRCE.png',
         'https://i.imgur.com/tnqJDWe.png'
       ],
-      checkout: 'https://blacknosepay.com/pay/C6DL86SN',
+      checkout: 'https://blacknosepay.com/pay/0Y57BDM7',
     },
   ];
 
