f92823c151a837dc756bf236a91e0526e2bab239
testethalysson1-lgtm <testethalysson1@gmail.com>
2026-04-05 14:50:36 -0300

    salvando antDSFSEFWERFSEFSDes do pull

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
         'https://i.imgur.com/ChkFVNG.jpeg',
         'https://i.imgur.com/ALZNfiQ.jpeg'
       ],
-      checkout: 'https://blacknosepay.com/pay/ZNI5QZCG',
+      checkout: 'https://blacknosepay.com/pay/R2XB3QE9',
     },
     {
       id: 4,
@@ -80,7 +80,7 @@ export default function Page() {
         'https://i.imgur.com/UZLuCvy.png',
         'https://i.imgur.com/XZVNLax.png'
       ],
-      checkout: 'https://blacknosepay.com/pay/CCNFPPDJ',
+      checkout: 'https://blacknosepay.com/pay/7BCYN85T',
     },
     {
       id: 5,
