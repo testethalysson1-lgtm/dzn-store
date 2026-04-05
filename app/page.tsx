e0437a5685af46fb70c4c45c2aa37ee5c378c252
testethalysson1-lgtm <testethalysson1@gmail.com>
2026-04-05 10:22:47 -0300

    atualizaçSASAão

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
         'https://i.imgur.com/ChkFVNG.jpeg',
         'https://i.imgur.com/ALZNfiQ.jpeg'
       ],
-      checkout: 'https://blacknosepay.com/pay/NMBX8B68',
+      checkout: 'https://blacknosepay.com/pay/95KFMMHP',
     },
     {
       id: 4,
