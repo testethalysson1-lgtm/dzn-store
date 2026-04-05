4c99bee4486b0ab7803ff063a5610cf7149df743
testethalysson1-lgtm <testethalysson1@gmail.com>
2026-04-05 14:05:37 -0300

    salvando antes DFSTRVTETdo pull

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
         'https://i.imgur.com/ChkFVNG.jpeg',
         'https://i.imgur.com/ALZNfiQ.jpeg'
       ],
-      checkout: 'https://blacknosepay.com/pay/4TVT9SKF',
+      checkout: 'https://blacknosepay.com/pay/4P40V1NO',
     },
     {
       id: 4,
